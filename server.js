01f1f1e84ccb3dd27f84cc013258dd399c971c0b
unnati2000 <unnatibamania8@gmail.com>
2021-08-26 21:55:09 +0530

    Add env variable

--- server.js
+++ server.js
@@ -17,10 +17,12 @@ app.use(require("./routes/order.routes"));
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("frontend/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
